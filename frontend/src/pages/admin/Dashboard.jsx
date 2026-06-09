1166333c682f81673744b60770b905d5b78d6789
luqman <luqmantrisnap@gmail.com>
2026-06-09 23:10:41 +0700

    test perubahan admin dashboard 1.2

--- frontend/src/pages/admin/Dashboard.jsx
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -44,7 +44,7 @@ export default function Dashboard() {
   }, []);
 
   return (
-    <div className="p-6">
+    <div className="p-6 text-left">
 
       {/* Header */}
       <div className="flex items-center justify-between mb-8">
